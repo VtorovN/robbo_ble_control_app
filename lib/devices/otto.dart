0910459313169f285023bda2d0e1bc824e2f9dd8
nikita_vtorov <s.nikita.vtorov@gmail.com>
2021-05-12 16:53:56 +0300

    Add simple otto interface

--- lib/devices/otto.dart
+++ lib/devices/otto.dart
@@ -1,14 +1,38 @@
-import 'command.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:ble_control_app/bluetooth/ble_api.dart';
 
-class Device {
-  Set<Command> _commands = new Set<Command>();
-  Command _currentCommand;
+class Otto {
+  BluetoothService _service;
 
-  Device() {
-    // TODO: make constructor
+  final String _serviceUUID = "fe16f2b0-7783-11eb-9881-0800200c9a66";
+  final String _blinkUUID = "69869f60-7788-11eb-9881-0800200c9a66";
+
+  Map<String, BluetoothCharacteristic> savedCharacteristics;
+
+  Otto() {
+    savedCharacteristics = { _blinkUUID : null };
   }
 
-  void runCommand(String commandID) { // or name?
-    // TODO: find command and run
+  Future _initService() async {
+    List<BluetoothService> services = await BLEAPI.instance.getServices();
+    _service = services.firstWhere((service) => service.uuid.toString() == _serviceUUID);
+  }
+
+  Future<BluetoothCharacteristic> getCharacteristicByID(String id) async {
+    if (_service == null) {
+      await _initService();
+    }
+
+    if (savedCharacteristics[id] != null) {
+      return savedCharacteristics[id];
+    }
+
+    return savedCharacteristics[id] =
+        _service.characteristics.firstWhere((characteristic) => characteristic.uuid.toString() == id);
+  }
+
+  void blink() async {
+    BluetoothCharacteristic characteristic = await getCharacteristicByID(_blinkUUID);
+    characteristic.write([49]);
   }
 }
