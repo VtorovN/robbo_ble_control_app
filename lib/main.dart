b85ba45a8e00ce19722cb9e5b1f7f0d0753413df
nikita_vtorov <s.nikita.vtorov@gmail.com>
2021-07-01 02:11:52 +0300

    Remove unused code from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:ble_control_app/bluetooth/scanner.dart';
 import 'package:ble_control_app/screens/home/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,29 +6,9 @@ import 'package:ble_control_app/screens/devices.dart';
 import 'package:ble_control_app/screens/scripts.dart';
 import 'package:ble_control_app/screens/settings.dart';
 import 'package:ble_control_app/screens/about.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final FlutterReactiveBle _ble = FlutterReactiveBle();
-  final BleScanner _scanner = BleScanner(_ble);
-
-  runApp(
-      MultiProvider(
-          providers: [
-            StreamProvider<BleScannerState>(
-              create: (_) => _scanner.state,
-              initialData: const BleScannerState(
-                discoveredDevices: [],
-                scanIsInProgress: false,
-              ),
-            ),
-          ],
-          child: App()
-      )
-  );
+  runApp(App());
 }
 
 class App extends StatelessWidget {
